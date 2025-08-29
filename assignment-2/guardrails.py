e6c185004eefac9554ff343b6254cb96137d23ce
Abdullah <abdullahwindows10user@gmail.com>
2025-08-30 01:16:06 +0500

    assignment-2 completed

--- assignment-2/guardrails.py
+++ assignment-2/guardrails.py
@@ -3,83 +3,97 @@ from openai import AsyncOpenAI
 from agents import (
     Agent,
     GuardrailFunctionOutput,
-    InputGuardrail,
     RunContextWrapper,
     Runner,
     OpenAIChatCompletionsModel,
     TResponseInputItem,
     input_guardrail,
-    set_tracing_export_api_key,
-    InputGuardrailTripwireTriggered,
+    output_guardrail,
+    InputGuardrailTripwireTriggered,OutputGuardrailTripwireTriggered,
+    set_tracing_disabled
 )
 from dotenv import find_dotenv, load_dotenv
-import os
-import asyncio
-
 from pydantic import BaseModel
+import os
 
 load_dotenv(find_dotenv(), override=True)
+set_tracing_disabled(True)
 
-api_key = os.getenv("OPENAI_API_KEY1")
-base_url = os.getenv("OPENAI_BASE_URL1")
-model_name = os.getenv("OPENAI_MODEL_NAME1")
+api_key = os.getenv("OLLAMA_API_KEY")
+base_url = "http://localhost:11434/v1"
+model_name = "gemma3:1b"
 
-
-client = AsyncOpenAI(api_key=api_key, base_url=base_url)
-model = OpenAIChatCompletionsModel(openai_client=client, model=model_name)
-
-set_tracing_export_api_key(api_key=api_key)
-
-
-class MathOutPut(BaseModel):
+class MathGuardrailOutput(BaseModel):
     is_math: bool
     reason: str
 
-@input_guardrail
-async def check_input(
-    ctx: RunContextWrapper[Any], agent: Agent[Any], input_data: str | list[TResponseInputItem]
-) -> GuardrailFunctionOutput:
-    # print("input_data : ", input_data)
+client = AsyncOpenAI(api_key=api_key, base_url=base_url)
 
-    input_agent = Agent(
-        "InputGuardrailAgent",
-        instructions="Check and verify if input is related to math",
+model = OpenAIChatCompletionsModel(openai_client=client, model=model_name)
+
+@input_guardrail
+async def math_input_guardrail(
+    ctx: RunContextWrapper[MathGuardrailOutput],
+    agent: Agent[MathGuardrailOutput],
+    input_data: str | list[TResponseInputItem]
+) -> GuardrailFunctionOutput:
+    math_input_guardrail_agent = Agent[MathGuardrailOutput](
+        name="InputGuardrailAgent",
+        instructions='''These are for your instructions, it's not the user prompt: "Output `is_math` to `True` only and only if the user prompt is directly related to 'MATHEMATICS' else set `is_math` to `False` and tell the reason why the `is_math` is `True` or `False`"''',
         model=model,
-        output_type=MathOutPut,
+        output_type=MathGuardrailOutput,
     )
-    result = await Runner.run(input_agent, input_data, context=ctx.context)
+
+    result = await Runner.run(math_input_guardrail_agent, f"User prompt: ```{input_data}```", context=ctx.context)
     final_output = result.final_output
-    # print(final_output)
+
+    print(f"\nInput\nIs math: {final_output.is_math}\nReason: {final_output.reason}")
 
     return GuardrailFunctionOutput(
-        output_info=final_output, tripwire_triggered=not final_output.is_math
+        output_info=final_output,
+        tripwire_triggered=not final_output.is_math
     )
 
+@output_guardrail
+async def math_output_guardrail(
+    ctx: RunContextWrapper[MathGuardrailOutput],
+    agent: Agent[MathGuardrailOutput],
+    input_data: str | list[TResponseInputItem]
+) -> GuardrailFunctionOutput:
+    math_input_guardrail_agent = Agent[MathGuardrailOutput](
+        name="OutputGuardrailAgent",
+        instructions='''These are for your instructions, it's not the agent response: "Output `is_math` to `True` only if the response contains MATHEMATICAL content and NO political topics or references to political figures. Set `is_math` to `False` if the response mentions any political content or figures. Provide the reason why `is_math` is `True` or `False`"''',
+        model=model,
+        output_type=MathGuardrailOutput,
+    )
+
+    result = await Runner.run(math_input_guardrail_agent, f"Agent response: ```{input_data}```", context=ctx.context)
+    final_output = result.final_output
+
+    print(f"\nOutput\nIs math: {final_output.is_math}\nReason: {final_output.reason}")
+
+    return GuardrailFunctionOutput(
+        output_info=final_output,
+        tripwire_triggered=not final_output.is_math
+    )
 
 math_agent = Agent(
-    "MathAgent",
+    name="MathAgent",
     instructions="You are a math agent",
     model=model,
-    input_guardrails=[check_input],
-    # input_guardrails=[InputGuardrail(guardrail_function=check_input)]
+    input_guardrails=[math_input_guardrail],
 )
 
-general_agent = Agent(
-    "GeneralAgent",
-    instructions="You are a helpful agent",
-    model=model,
-    #output_guardrails=
-)
+class MathOutput(BaseModel):
+    result: int
 
-
-async def main():
+def main():
     try:
-        # a = 10/0
-        msg = input("Enter you question : ")
-        result = await Runner.run(general_agent, msg)
-        print(f"\n\n final output : {result.final_output}")
-    except InputGuardrailTripwireTriggered as ex:
-        print("Error : invalid prompt")
+        msg = input("Enter you question: ")
+        result = Runner.run_sync(math_agent, msg)
+        print(f"\nFinal output: {result.final_output}")
 
+    except (InputGuardrailTripwireTriggered, OutputGuardrailTripwireTriggered) as output:
+        print(f"Error: {output}")
 
-asyncio.run(main())
+main()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from q3-assignments!")
-
-
-if __name__ == "__main__":
-    main()
